4e8b3fd62a501385feeadb5b04905c7ee680210c
Joanne Lee <lee.joanne54@gmail.com>
2023-05-16 15:17:22 -0700

    Add timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,10 +22,30 @@ window.onclick = function (event) {
 let startMenu = document.getElementById("start-menu");
 let startBtn = document.getElementById("startBtn");
 let gameContainer = document.getElementById("memory-game");
+let counter = document.getElementById("timer");
 
 startBtn.addEventListener("click", startGame);
 
 function startGame() {
     startMenu.classList.add("hide");
     gameContainer.classList.remove("hide");
+    countdown(2, 30);
+}
+
+function countdown(minutes, seconds) {
+    function tick() {
+        counter.innerHTML =
+            minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        seconds--;
+        if (seconds >= 0) {
+            timeoutHandle = setTimeout(tick, 1000);
+        } else {
+            if (minutes >= 1) {
+                setTimeout(function () {
+                    countdown(minutes - 1, 59);
+                }, 1000);
+            }
+        }
+    }
+    tick();
 }
