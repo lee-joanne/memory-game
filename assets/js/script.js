f02714e2c8fa0b708fdd3f1a67035a2f64fdda4f
Joanne Lee <lee.joanne54@gmail.com>
2023-05-16 08:57:47 -0700

    Add cards to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,12 @@ window.onclick = function (event) {
 // game functionailty
 
 let startMenu = document.getElementById("start-menu");
+let startBtn = document.getElementById("startBtn");
+let gameContainer = document.getElementById("memory-game");
+
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+    startMenu.classList.add("hide");
+    gameContainer.classList.remove("hide");
+}
