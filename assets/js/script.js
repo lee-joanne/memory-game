a985506bffe2b904ecd96428f3ca5745cfa7a35c
Joanne Lee <lee.joanne54@gmail.com>
2023-08-28 11:17:42 -0700

    Complete memory game, fix bug where previous matched card are clickable and added strings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-// modal window functionality
+// Modal window functionality
 let modal = document.getElementById("modalContainer");
 let modalBtn = document.getElementById("modalBtn");
 let span = document.getElementsByClassName("close")[0];
-let ping = new Audio('../assets/audio/ding.mp3');
 
 modalBtn.onclick = function () {
     modal.style.display = "block";
@@ -18,7 +17,7 @@ window.onclick = function (event) {
     }
 }
 
-// game functionailty
+// Game functionailty
 let titleScreen = document.getElementById("centering")
 let startMenu = document.getElementById("start-menu");
 let startBtn = document.getElementById("startBtn");
@@ -29,8 +28,12 @@ let gameOverPage = document.getElementById("game-over-page");
 let turnCount = document.getElementById("turns");
 let finalCount = document.getElementById("final-count");
 let wellDonePage = document.getElementById("well-done-page");
+let ping = new Audio('../assets/audio/ding.mp3');
 
 function startGame() {
+    // Function to start the game.
+    // Hides start menu screen and shows game container.
+    // Starts countdown, passing in two minutes and 30 seconds
     startMenu.classList.add("hide");
     gameContainer.classList.remove("hide");
     countdown(2, 30);
@@ -38,16 +41,8 @@ function startGame() {
 
 startBtn.addEventListener("click", startGame);
 
-function flipCards() {
-    // functionality to flip the cards
-    this.classList.toggle('flip');
-}
-
-for (let i = 0; i < memoryCards.length; i++) {
-    memoryCards[i].addEventListener("click", flipCards);
-}
-
 function countdown(minutes, seconds) {
+    // Countdown, two minutes and 30 seconds passed in from startGame function.
     function tick() {
         counter.innerHTML =
             minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
@@ -62,6 +57,7 @@ function countdown(minutes, seconds) {
             }
         }
         if (counter.innerText === "0:00") {
+                // if countdown is over, show gameover screen.
                 gameOverPage.classList.remove("hide");
                 gameContainer.classList.add("hide");
                 titleScreen.classList.add("hide")
@@ -71,35 +67,36 @@ function countdown(minutes, seconds) {
 }
 
 const cards = document.querySelectorAll('.memory-card');
-let hasFlippedCard = false; // card not flipped yet
-let firstCard, secondCard;
-let confirmedFirst, confirmedSecond;
-let selectedCardsCount = 0;
-let matchedCards = 0;
+let hasFlippedCard = false; // Card not flipped yet.
+let firstCard, secondCard; // Declare firstCard and secondCard, set to null.
+let selectedCardsCount = 0; // Initial declaration, 0 cards selected.
+let matchedCards = 0; // Initial declaration, 0 cards matched.
 
 function incrementTurns() {
+    // Increment turns for each pair, show turn counts on website.
     let oldTurn = parseInt(turnCount.innerText);
     turnCount.innerText = ++oldTurn;
 }
 
 function flipCard() {
-    if (this === firstCard) return;
-
+    // Function to flip a pair of cards.
+    // Add classList of 'flip' to the element calling this function.
+    // If hasFlippedCard is false (not flipped yet), declare this element as firstCard.
+    // hasFlippedCard is now set to true.
+    // When another element calls this function, declare this as the secondCard, skipping if statement.
+    // Add class list of 'disabled' to second card, and set hasFlippedCard back to false.
+    // Increment counter of selectedCardsCount.
+    // If selectedCardsCount is 1, call disableRemainingCards function.
+    // Check to see if the cards match with checkForMatch function.
     this.classList.add('flip');
-
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-        firstCard.classList.add("disabled");
         return;
     }
-
     secondCard = this;
-    secondCard.classList.add("disabled");
     hasFlippedCard = false;
-
     selectedCardsCount++; // Increment the counter
-
     if (selectedCardsCount == 1) {
         disableRemainingCards();
     }
@@ -107,7 +104,8 @@ function flipCard() {
 }
 
 function disableRemainingCards() {
-    console.log("disabling cards...")
+    // For each card, if the card is not firstCard or secondCard, remove event listener and disable cards.
+    // Function created to stop users from selecting multiple cards when checking for match.
     cards.forEach(card => {
         if (card !== firstCard && card !== secondCard) {
             card.removeEventListener('click', flipCard);
@@ -117,27 +115,32 @@ function disableRemainingCards() {
 }
 
 function checkForMatch() {
+    // Function to run to check of two cards match.
+    // Check for match using the dataset framework.
+    // Call the incrementTurns function to increment turn number.
+    // If it's a match, call disableCards. Else, call unFlipCards.
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     incrementTurns();
     isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
+    // Function to run when cards are a match.
+    // Play song.
+    // Increment matchedCards by one.
+    // If matchedCards is 18, call the wellDone function.
+    // resetBoard function called.
     ping.play();
-    confirmedFirst = firstCard
-    confirmedSecond = secondCard
-    confirmedFirst.classList.add('disabled');
-    confirmedSecond.classList.add('disabled');
     matchedCards += 1;
-    console.log(matchedCards);
     if (matchedCards == 18) {
-        console.log("Done");
         wellDone();
     }
     resetBoard();
 }
 
 function unflipCards() {
+    // Function to run if cards do not match.
+    // Remove flip class and call resetBoard function when timer ends.
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
@@ -146,19 +149,22 @@ function unflipCards() {
 }
 
 function resetBoard() {
+    // Function to reset the board.
+    // Set firstCard and secondCard back to null.
+    // Reset selectedCardsCount counter to 0.
+    // If cards do not have a class of 'flip', allow cards to be clickable again.
     [firstCard, secondCard] = [null, null];
-    hasFlippedCard = false;
     selectedCardsCount = 0; // Reset the counter
     cards.forEach(card => {
-        if (card !== confirmedFirst && card !== confirmedSecond) {
+        if (!card.classList.contains("flip")) {
             card.addEventListener('click', flipCard);
             card.classList.remove('disabled');
     }
     });
-    console.log("Resetting...")
 }
 
 (function shuffle() {
+    // Function to shuffle the cards, will run when website loads.
    cards.forEach(card => {
      let ramdomPos = Math.floor(Math.random() * 12);
      card.style.order = ramdomPos;
@@ -168,7 +174,9 @@ function resetBoard() {
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function wellDone() {
-    console.log("This function is running")
+    // Function to show well done page.
+    // Shows final score to the user.
+    // Shows well done page and hides game container and title screen.
     finalCount.innerText = turnCount.innerText;
     wellDonePage.classList.remove("hide");
     gameContainer.classList.add("hide");
